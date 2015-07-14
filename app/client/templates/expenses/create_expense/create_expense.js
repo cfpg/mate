c3b979ec3d3c9638497e9406d3667869dbdcfaac
Ricardo Fornes <jrfornes5@gmail.com>
2015-07-13 22:17:49 -0700

    Listening to submit events on create expense form

--- app/client/templates/expenses/create_expense/create_expense.js
+++ app/client/templates/expenses/create_expense/create_expense.js
@@ -2,6 +2,24 @@
 /* CreateExpense: Event Handlers */
 /*****************************************************************************/
 Template.CreateExpense.events({
+  
+  'submit .create-expense': function(e){
+    
+    if (e) e.preventDefault();
+    
+    var subject = e.target.text.value;
+
+    console.log('subject', subject);
+    console.log('typeof', typeof subject);
+
+    Expenses.insert({
+      'subject': subject,
+      'amount': 130,
+      'payer': 1
+    });
+
+  }
+
 });
 
 /*****************************************************************************/
@@ -14,9 +32,11 @@ Template.CreateExpense.helpers({
 /* CreateExpense: Lifecycle Hooks */
 /*****************************************************************************/
 Template.CreateExpense.created = function () {
+  console.log('created');
 };
 
 Template.CreateExpense.rendered = function () {
+  console.log('rendered form');
 };
 
 Template.CreateExpense.destroyed = function () {
