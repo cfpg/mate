85f9f6e2d9a9fedd9db8f211dc7cc3f8b2d528cd
Carlos F. Peña <carlos@sutil.mx>
2015-07-13 22:45:37 -0700

    Account login with route auth

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -24,3 +24,40 @@ Router.route('/expenses/create', {
   action:'create',
   where:'client'
 });
+
+Router.route('/login', {
+  name:'Login',
+  controller:'LoginController',
+  action:'login',
+  where:'client'
+});
+
+Router.route('/logout', {
+  name:'Logout',
+  controller:'LoginController',
+  action:'logout',
+  where:'client'
+});
+
+// (Global) Before hooks for any route
+var beforeHooks = {
+    isLoggedIn: function(pause) {
+        if (!(Meteor.loggingIn() || Meteor.user())) {
+					console.log(Router.current())
+					if (Router.current().route.getName() == 'Login') {
+						this.next();
+					}
+					var snd = new Audio("http://wavcentral.com/sounds/movies/jurassic/jurass01.mp3");
+					var t = 500;
+					setInterval(function(){
+						snd.play();
+						t = (t/2) - (t/3);
+					},500);
+          Notifications.error('You need an account for that!', 'Please login');
+          Router.go('Login');
+        } else {
+        	this.next();
+        }
+    }
+}
+Router.onBeforeAction(beforeHooks.isLoggedIn);
