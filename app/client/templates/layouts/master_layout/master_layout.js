abd773497a9e59f0ee464783b974124dc0b1455e
Carlos F. Peña <carlos@sutil.mx>
2015-07-13 23:31:18 -0700

    Added layout & bootstrap

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -2,4 +2,37 @@ Template.MasterLayout.helpers({
 });
 
 Template.MasterLayout.events({
+	'click a.navbar-nav li a':function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		var link = $(event.target);
+		var goTo = $link.data('goto');
+		console.log(link)
+	}
+});
+
+function htmlbodyHeightUpdate(){
+	var height3 = $( window ).height()
+	var height1 = $('.nav').height()+50
+	height2 = $('.main').height()
+	if(height2 > height3){
+		$('html').height(Math.max(height1,height3,height2)+10);
+		$('body').height(Math.max(height1,height3,height2)+10);
+	}
+	else
+	{
+		$('html').height(Math.max(height1,height3,height2));
+		$('body').height(Math.max(height1,height3,height2));
+	}
+	
+}
+$(document).ready(function () {
+	htmlbodyHeightUpdate()
+	$( window ).resize(function() {
+		htmlbodyHeightUpdate()
+	});
+	$( window ).scroll(function() {
+		height2 = $('.main').height()
+			htmlbodyHeightUpdate()
+	});
 });
